7de714fe870a97709d9d9de456a7be9d094a0f0f
Jonathan Keeling <jmjk@st-andrews.ac.uk>
2024-07-15 17:48:45 +0100

    Making code and output more similar to direct version

--- run_spin_matrix_elements.py
+++ run_spin_matrix_elements.py
@@ -124,7 +124,7 @@ for spin_element_index in range(num_elements):
     left, right = np.split(element,2)
     #Olambda = get_rdm(spin_element_index) # no need to actually compute RDM
     rdm_coords = get_rdm_indices(left, right)# Positions of 1s in O_lambda
-    partitions = get_partitions(left,right)
+    partition = get_partitions(left,right)
     m_left, m_right = sum(left), sum(right) # Count number of DOWN (1) spins
     sk_lefts, sk_rights = m_index[:,m_left], m_index[:,m_right]
     allowed_s = np.where((sk_lefts >= 0) & (sk_rights >= 0))[0]
@@ -156,7 +156,8 @@ for spin_element_index in range(num_elements):
         #        print(f'lambda={spin_element_index:4d}, partitions={partitions}, count={len(rdm_coords):4d},    ml={m_left}, mr={m_right}, NCml={NCml:3d}, NCmr={NCmr:3d}, sigma={sigma[s]},    overlap=sqrt({num_sq_int/gcd_val:.0f}/{den_sq_int/gcd_val:.0f})')
         #print(f'lambda={spin_element_index:4d}, partitions={partitions}, count={len(rdm_coords):4d},    ml={m_left}, mr={m_right}, NCml={NCml:3d}, NCmr={NCmr:3d}, sigma={sigma[s]}, S={sigma[s][-1]}    overlap=sqrt({overlap**2:.2f})')
         if (sigma[s][-1] != old_Stot):
-            print(f'lambda={spin_element_index:4d}, partitions={partitions},  S={sigma[s][-1]}    overlap=sqrt({overlap**2:.2f})')
+            Stot=sigma[s][-1]/2.0
+            print(f'lambda={spin_element_index:4d}, partitions={partition}, S={Stot},    overlap=sqrt({overlap**2:.2f})')
         old_Stot=sigma[s][-1]
 
         
